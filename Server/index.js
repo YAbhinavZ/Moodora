5a2ae2a15b6ec73c05840f8ef74f78dcd1510685
YAbhinavZ <abhinavvirtuoso@gmail.com>
2026-06-30 23:58:40 +0530

    database connection

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,18 @@
 import express from "express";
-import dotenv from "dotenv"
-import mongoose from "mongoose";
-import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
-const app = express();
+
 dotenv.config();
 
-app.use(cors());
+connectDB();
+
+const app = express();
+
 app.use(express.json());
 
-const PORT = 8000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log("SERVER IS LISTENING");
-})
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
