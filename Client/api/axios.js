b0c947c1649cadb5397de6dab8ad4e8b66436578
YAbhinavZ <abhinavvirtuoso@gmail.com>
2026-07-10 22:36:50 +0530

    fixes

--- Client/api/axios.js
+++ Client/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL:"http://localhost:8000/api",
 });
 
 // Attach JWT token to every request automatically
