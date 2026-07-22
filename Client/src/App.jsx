ef89f02a13d483405e875b6605ba5ebc675fe937
YAbhinavZ <abhinavvirtuoso@gmail.com>
2026-07-22 23:47:45 +0530

    Clearing some stuff

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,67 @@
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AuthProvider } from "../context/AuthContext";
+import Navbar from "../components/Navbar";
+import ProtectedRoute from "../components/ProtectedRoute";
 
-import './App.css'
+import Home from "../pages/Home";
+import Dashboard from "../pages/DashBoard";
+import History from "../pages/History";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
-function App() {
+function AppContent() {
+  const location = useLocation();
 
+  // Hide navbar on auth pages
+  const hideNavbar = ["/login", "/register"].includes(location.pathname);
 
   return (
     <>
-      <h2>Hello World</h2>
+      {!hideNavbar && <Navbar />}
+
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/history"
+          element={
+            <ProtectedRoute>
+              <History />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <AppContent />
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
